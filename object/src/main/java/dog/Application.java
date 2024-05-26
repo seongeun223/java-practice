96361045757976f583e8e17ecc1471e719857a4d
seongeun223 <jklun080921@gmail.com>
2024-05-26 21:21:43 +0900

    dog

--- object/src/main/java/dog/Application.java
+++ object/src/main/java/dog/Application.java
@@ -3,5 +3,11 @@ package dog;
 public class Application {
     public static void main(String[] args) {
 
+        Dog dog = new Dog();
+        dog.setName("MAX");
+        dog.setAge(5);
+        dog.setKind("골든리트리버");
+
+        System.out.println(dog.getInfo());
     }
 }
--- object/src/main/java/dog/Dog.java
+++ object/src/main/java/dog/Dog.java
@@ -2,4 +2,25 @@ package dog;
 
 public class Dog {
 
+    private String name;
+    private int age;
+    private String kind;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getInfo() {
+
+        return "강아지의 이름은 " + this.name + "이고 " + this.age + "살이며 종은 " + this.kind + "이다.";
+    }
+
 }
